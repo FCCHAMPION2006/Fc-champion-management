402987e7f3bf989be6fc125825bd9198d55f3c78
FCCHAMPION2006 <princejoubertjoseph@gmail.com>
2026-09-15 20:56:47 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="fc-champion-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icone-192.png","./icone-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
